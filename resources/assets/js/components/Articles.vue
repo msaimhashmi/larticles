a45e726dcceea8be5a3a7a2561c2f668846f5fed
saimhashmii <saimhashmii433@gmail.com>
2022-03-11 20:00:09 +0500

    First

---
+++ resources/assets/js/components/Articles.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h2>Saim</h2>
+	</div>
+</template>
+<!-- <script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script> -->
